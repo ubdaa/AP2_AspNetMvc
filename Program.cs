7c8b8f57b9a61408bdbcf6d933de09b937b1d58b
ubdaa <123176831+ubdaa@users.noreply.github.com>
2024-10-03 17:20:00 +0200

    ajout de tailwindcss

--- Program.cs
+++ Program.cs
@@ -58,7 +58,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Account}/{action=Index}/{id?}"
+    pattern: "{controller=Patient}/{action=Index}/{id?}"
     );
 
 app.Run();
