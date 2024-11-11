86ab3f0e99383369c494aed0c1f6cedd535bd920
Ubdaa <123176831+ubdaa@users.noreply.github.com>
2024-11-11 16:31:05 +0100

    début dashboard avec données bidons

---
+++ ViewModel/Dashboard/DashboardViewModel.cs
@@ -0,0 +1,8 @@
+namespace MedManager.ViewModel.Dashboard;
+
+public class DashboardViewModel
+{
+    public List<Models.Prescription> Prescriptions { get; set; }
+    public List<Models.Patient> Patients { get; set; }
+    
+}
