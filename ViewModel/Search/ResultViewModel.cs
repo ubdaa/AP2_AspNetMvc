6b35a85ec14e67544d6a46a7c462a0d7a8fb6d34
ubdaa <123176831+ubdaa@users.noreply.github.com>
2024-11-15 11:30:07 +0100

    ajout de la table patient en composant

--- ViewModel/Search/ResultViewModel.cs
+++ ViewModel/Search/ResultViewModel.cs
@@ -2,9 +2,9 @@ namespace MedManager.ViewModel.Search;
 
 public class ResultViewModel
 {
-    public List<Models.Patient> Patients { get; set; }
-    public List<Models.Allergy> Allergies { get; set; }
-    public List<Models.MedicalHistory> MedicalHistories { get; set; }
-    public List<Models.Prescription> Prescriptions { get; set; }
-    public List<Models.Medicament> Medicaments { get; set; }
+    public List<Models.Patient> Patients { get; set; } = new();
+    public List<Models.Allergy> Allergies { get; set; } = new();
+    public List<Models.MedicalHistory> MedicalHistories { get; set; } = new();
+    public List<Models.Prescription> Prescriptions { get; set; } = new();
+    public List<Models.Medicament> Medicaments { get; set; } = new();
 }
